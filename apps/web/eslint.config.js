213975fd92c5c7283553259c7c1557d1e33f291b
haloivanid <ivanazis.id@gmail.com>
2025-03-13 00:08:23 +0700

    feat(api): initialize

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -3,10 +3,12 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
+    prettier,
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
